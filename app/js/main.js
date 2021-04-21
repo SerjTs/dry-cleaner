34333ed50cb08ac7e7037342f859152eae1e338f
SerjTs <postwork@mail.ru>
2021-04-21 10:13:37 +0300

    rebuild calculator section

--- app/js/main.js
+++ app/js/main.js
@@ -71,7 +71,167 @@ $(document).ready(function () {
     $('body,html').animate({ scrollTop: top }, 1000);
   });
 
+  // plus & minus input
+  // Убавляем кол-во по клику
+  $('.counter #buttonCountMinus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) - 1;
+    count = count < 0 ? 0 : count;
+    $input.val(count);
+  });
+  // Прибавляем кол-во по клику
+  $('.counter #buttonCountPlus').click(function () {
+    let $input = $(this).parent().find('.quantity');
+    let count = parseInt($input.val()) + 1;
+    count = count > parseInt($input.data('max-count')) ? parseInt($input.data('max-count')) : count;
+    $input.val(parseInt(count));
+    // console.log($input.val());
+  });
+  // Убираем все лишнее и невозможное при изменении поля
+  $('.counter .quantity').bind("change keyup input click", function () {
+    if (this.value.match(/[^0-9]/g)) {
+      this.value = this.value.replace(/[^0-9]/g, '');
+    }
+    if (this.value == "") {
+      this.value = 1;
+    }
+    if (this.value > parseInt($(this).data('max-count'))) {
+      this.value = parseInt($(this).data('max-count'));
+    }
+  });
 
+  // Калькулятор
+  function ResultCalculator(){
+        result = 0;
+        
+        calk_1 = $(".calk_1").attr("data-price"); // толщина листа
+        
+        calk_2 = $(".calk_2").attr("data-price"); // высота забора
+        
+        calk_3 = $(".calk_3").attr("data-price"); // покрытие
+        
+        dlina = $(".calk_4").val(); // длина забора
+        dlina_1000 = parseInt(dlina) * 1000; // 1 м = 1000 руб
+        
+        calk_5 = $(".calk_5").attr("data-price"); // наличие ворот
+        
+        calk_6 = $(".calk_6").attr("data-price"); // наличие калиток
+        
+        lagi = $(".calk_7").attr("data-price"); // забор укрепления лагами
+        
+        calk_8 = $(".calk_8").attr("data-price"); // направление доставки
+        
+        calk_9 = $(".calk_9").val(); // количество километров от мкад
+        
+        
+        if(calk_1 == "0"){
+            calk_1_result = 0;
+        } else{
+            calk_1_result = 100;
+        }
+        result += parseInt(dlina_1000) + ( parseInt(dlina) * parseInt(calk_1_result) );
+        
+        if(lagi == "0"){
+            lagi_result = 0;
+        } else{
+            lagi_result = lagi;
+        }
+        
+        result += parseInt(dlina) * parseInt(lagi_result);
+        
+        
+        if(calk_2 == "0"){
+            calk_2_result = 0;
+        } else{
+            calk_2_result = calk_2;
+        }
+        
+        result += dlina * calk_2_result;
+        
+        
+        if(calk_3 == "0"){
+            calk_3_result = 0;
+        } else{
+            calk_3_result = calk_3;
+        }
+        
+        result += dlina * calk_3_result + parseInt(calk_5) + parseInt(calk_6);
+        
+        result += parseInt(calk_8) * parseInt(calk_9);
+        
+        $(".result_calculator span").text(result); // ВСЕГО
+    }
+    
+    $(".r_calc").click(function(){
+        $(this).next().stop(true, true).slideToggle("10");
+    })
+    
+    $(".options_calc div").click(function(){
+        var data_text = $(this).text();
+        var data_price = $(this).attr("data-price");
+        $(this).parent().prev().text(data_text).attr("data-price", data_price);
+        $(this).parent().stop(true, true).slideToggle("10");
+        $(this).parent().children(".active").removeClass("active");
+        $(this).addClass("active");
+        ResultCalculator();
+    })
+    
+    $( ".too, .tooo" ).keyup(function(){
+        ResultCalculator();
+    });
+    
+    $(document).mouseup(function (e) {
+        var container = $(".options_calc");
+        if (container.has(e.target).length === 0){
+            $(".options_calc").fadeOut("slow");
+        }
+    })
+    
+    $(".go_mail").click(function(){
+        
+        calk_1 = $(".calk_1").attr("data-price");
+        calk_1_text = $(".calk_1").text();
+        
+        calk_2 = $(".calk_2").attr("data-price");
+        calk_2_text = $(".calk_2").text();
+        
+        calk_3 = $(".calk_3").attr("data-price");
+        calk_3_text = $(".calk_3").text();
+        
+        calk_4 = $(".calk_4").val();
+        
+        calk_5 = $(".calk_5").attr("data-price");
+        calk_5_text = $(".calk_5").text();
+        
+        calk_6 = $(".calk_6").attr("data-price");
+        calk_6_text = $(".calk_6").text();
+        
+        calk_7 = $(".calk_7").attr("data-price");
+        calk_7_text = $(".calk_7").text();
+        
+        calk_8 = $(".calk_8").attr("data-price");
+        calk_8_text = $(".calk_8").text();
+        
+        calk_9 = $(".calk_9").val();
+        
+        result_calculator = $(".result_calculator span").text();
+        
+        email = $(".calk_email").val();
+        
+        $.ajax({
+            type:'post',
+            url:'action.php',
+            data:{'email':email,'calk_1':calk_1,'calk_1_text':calk_1_text,'calk_2':calk_2,'calk_2_text':calk_2_text,
+            'calk_3':calk_3,'calk_3_text':calk_3_text,'calk_4':calk_4,'calk_5':calk_5,'calk_5_text':calk_5_text,'calk_6':calk_6,'calk_6_text':calk_6_text,
+            'calk_7':calk_7,'calk_7_text':calk_7_text,'calk_8':calk_8,'calk_8_text':calk_8_text,'calk_9':calk_9,'result_calculator':result_calculator},
+            response:'text',
+            success:function(data){
+                $(".answer").text(data).fadeIn("slow");
+                $(".answer").delay(1000).fadeOut("slow");
+            }
+        })
+    })
+    // /Калькулятор
 
 
 });
